ee7a6126354709da9a0cfd47b30a511338edd12a
ilya usoltsev <i.usoltsev@life-stream.tv>
2021-01-01 16:52:45 +0300

    react-persist

--- src/redux/root-reducer.ts
+++ src/redux/root-reducer.ts
@@ -1,12 +1,20 @@
 import { combineReducers } from "redux";
 import cartReducer from "./cart/cart.reducer";
 import userReducer from "./user/user.reducer";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
 
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
 
 export type IRootState = ReturnType<typeof rootReducer>;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,14 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import rootReducer from "./root-reducer";
+import { persistStore } from "redux-persist";
 
 const middlewares = [logger];
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persitor = persistStore(store);
+
+const exportedStore = { store, persitor };
+
+export default exportedStore;
